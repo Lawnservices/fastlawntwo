b44105f6c25e5dd51353bf9e004469333378fdb2
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-09-19 17:29:39 -0400

    fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const laguna = document.getElementById('header');
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
     if (scroll_position > 240) {
-        header.style.backgroundColor = 'rgb(160,200, 180,0.4)'
+        header.style.backgroundColor = 'rgb(160,200, 180,0.6)'
     } else {
         header.style.backgroundColor = '#154'
     }
